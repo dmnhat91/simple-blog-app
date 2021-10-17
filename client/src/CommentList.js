67a5a8541e6a22c91f87da4c428e519492adbd49
dmnhat91 <henry.duong02@gmail.com>
2021-10-17 13:59:00 +0700

    Fix comment moderation to display status reflection on UI

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -18,7 +18,23 @@ const CommentList = ({comments}) => {
     // }, []);
 
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>;
+        let content;
+
+        if (comment.status === 'approved') {
+            content = comment.content;
+        }
+
+        if (comment.status === 'pending'){
+            content = 'This comment is awaiting moderation';
+        }
+
+        if (comment.status === 'rejected'){
+            content = 'This comment has been rejected';
+        }
+
+
+        // return <li key={comment.id}>{comment.content}</li>;
+        return <li key={comment.id}>{content}</li>;
     });
 
     return <ul>
